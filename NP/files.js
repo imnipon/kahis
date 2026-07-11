69f989fe0556ba430f779d00ad059b655dd7295a
Nipon Wongprasert Home <forregistertest@gmail.com>
2026-07-11 08:50:51 +0700

    update explorer, recent sessions, and generated files

---
+++ NP/files.js
@@ -0,0 +1,93 @@
+const filesTree = [
+  {
+    "name": "resource",
+    "type": "folder",
+    "path": "resource",
+    "children": [
+      {
+        "name": "landing-page.png",
+        "type": "file",
+        "path": "resource/landing-page.png"
+      },
+      {
+        "name": "resource_subj-editor.jpg",
+        "type": "file",
+        "path": "resource/resource_subj-editor.jpg"
+      }
+    ]
+  },
+  {
+    "name": "NP",
+    "type": "folder",
+    "path": "NP",
+    "children": [
+      {
+        "name": "assessment",
+        "type": "folder",
+        "path": "NP/assessment",
+        "children": [
+          {
+            "name": "assessment_editor.html",
+            "type": "file",
+            "path": "NP/assessment/assessment_editor.html",
+            "lastUpdate": "2026-07-11T06:11:13.415010"
+          },
+          {
+            "name": "checkin-form.html",
+            "type": "file",
+            "path": "NP/assessment/checkin-form.html",
+            "lastUpdate": "2026-07-08T11:05:40.162318"
+          },
+          {
+            "name": "drug-prescription-detail.html",
+            "type": "file",
+            "path": "NP/assessment/drug-prescription-detail.html",
+            "lastUpdate": "2026-07-08T18:26:21.450085"
+          },
+          {
+            "name": "index.html",
+            "type": "file",
+            "path": "NP/assessment/index.html",
+            "lastUpdate": "2026-07-11T07:17:37.827869"
+          },
+          {
+            "name": "lab-editor.html",
+            "type": "file",
+            "path": "NP/assessment/lab-editor.html",
+            "lastUpdate": "2026-07-08T11:14:32.571201"
+          },
+          {
+            "name": "list-of-visit.html",
+            "type": "file",
+            "path": "NP/assessment/list-of-visit.html",
+            "lastUpdate": "2026-07-08T11:45:28.935409"
+          },
+          {
+            "name": "order-rx.html",
+            "type": "file",
+            "path": "NP/assessment/order-rx.html",
+            "lastUpdate": "2026-07-08T17:33:56.783897"
+          },
+          {
+            "name": "order-slip-lab.html",
+            "type": "file",
+            "path": "NP/assessment/order-slip-lab.html",
+            "lastUpdate": "2026-07-08T19:21:38.893409"
+          },
+          {
+            "name": "order-slip-rx.html",
+            "type": "file",
+            "path": "NP/assessment/order-slip-rx.html",
+            "lastUpdate": "2026-07-08T17:30:21.286094"
+          },
+          {
+            "name": "search-hn.html",
+            "type": "file",
+            "path": "NP/assessment/search-hn.html",
+            "lastUpdate": "2026-07-08T11:08:11.343821"
+          }
+        ]
+      }
+    ]
+  }
+];
--- NP/generate-list.py
+++ NP/generate-list.py
@@ -1,9 +1,12 @@
 import os
+import json
 from datetime import datetime
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 ASSESSMENT_DIR = os.path.join(BASE_DIR, 'assessment')
 LIST_FILE = os.path.join(BASE_DIR, 'list.js')
+FILES_FILE = os.path.join(BASE_DIR, 'files.js')
 
 TITLE_MAP = {
     'assessment_editor': 'Assessment Editor',
@@ -25,15 +28,79 @@ def generate_title(name):
     return name.replace('-', ' ').replace('_', ' ').title()
 
 
+def get_last_update(path):
+    if not os.path.isfile(path):
+        return None
+    return datetime.fromtimestamp(os.path.getmtime(path)).isoformat()
+
+
+def build_tree(path, include_filter=None):
+    if not os.path.isdir(path):
+        return None
+    children = []
+    for name in sorted(os.listdir(path)):
+        if name.startswith('.'):
+            continue
+        full_path = os.path.join(path, name)
+        rel_path = os.path.relpath(full_path, ROOT_DIR).replace('\\', '/')
+        if os.path.isdir(full_path):
+            subtree = build_tree(full_path, include_filter)
+            if subtree:
+                children.append({
+                    'name': name,
+                    'type': 'folder',
+                    'path': rel_path,
+                    'children': subtree,
+                })
+        else:
+            if include_filter and not include_filter(name):
+                continue
+            node = {
+                'name': name,
+                'type': 'file',
+                'path': rel_path,
+            }
+            if name.endswith('.html'):
+                node['lastUpdate'] = get_last_update(full_path)
+            children.append(node)
+    return children
+
+
 def main():
+    files_tree = []
+    resource_dir = os.path.join(ROOT_DIR, 'resource')
+    np_dir = os.path.join(ROOT_DIR, 'NP')
+
+    if os.path.isdir(resource_dir):
+        resource_children = build_tree(resource_dir)
+        if resource_children:
+            files_tree.append({
+                'name': 'resource',
+                'type': 'folder',
+                'path': 'resource',
+                'children': resource_children,
+            })
+
+    if os.path.isdir(np_dir):
+        np_children = build_tree(np_dir, lambda name: name.endswith('.html'))
+        if np_children:
+            files_tree.append({
+                'name': 'NP',
+                'type': 'folder',
+                'path': 'NP',
+                'children': np_children,
+            })
+
+    with open(FILES_FILE, 'w', encoding='utf-8') as f:
+        f.write('const filesTree = ' + json.dumps(files_tree, indent=2, ensure_ascii=False) + ';\n')
+
     files = []
     if os.path.isdir(ASSESSMENT_DIR):
         for filename in sorted(os.listdir(ASSESSMENT_DIR)):
             if filename.endswith('.html'):
                 name = filename[:-5]
                 path = os.path.join(ASSESSMENT_DIR, filename)
-                mtime = os.path.getmtime(path)
-                dt = datetime.fromtimestamp(mtime).isoformat()
+                dt = get_last_update(path)
                 files.append({
                     'title': generate_title(name),
                     'file': f'NP/assessment/{filename}',
@@ -48,7 +115,7 @@ def main():
             )
         f.write('];\n')
 
-    print(f'Generated {LIST_FILE} with {len(files)} files.')
+    print(f'Generated {FILES_FILE} and {LIST_FILE} with {len(files)} assessment files.')
 
 
 if __name__ == '__main__':
