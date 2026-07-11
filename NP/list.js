bbb0e991596bb23194f73d5aa4c79200192e2bff
Nipon Wongprasert Home <forregistertest@gmail.com>
2026-07-11 08:12:22 +0700

    feat: landing page with notes, sessions, import/export and lastUpdate

---
+++ NP/list.js
@@ -0,0 +1,12 @@
+const assessmentFiles = [
+  { title: "Assessment Editor", file: "NP/assessment/assessment_editor.html", lastUpdate: "2026-07-11T06:11:13.415010" },
+  { title: "Check-in Form", file: "NP/assessment/checkin-form.html", lastUpdate: "2026-07-08T11:05:40.162318" },
+  { title: "Drug Prescription Detail", file: "NP/assessment/drug-prescription-detail.html", lastUpdate: "2026-07-08T18:26:21.450085" },
+  { title: "Assessment Home", file: "NP/assessment/index.html", lastUpdate: "2026-07-11T07:17:37.827869" },
+  { title: "Lab Editor", file: "NP/assessment/lab-editor.html", lastUpdate: "2026-07-08T11:14:32.571201" },
+  { title: "List of Visit", file: "NP/assessment/list-of-visit.html", lastUpdate: "2026-07-08T11:45:28.935409" },
+  { title: "Order Rx", file: "NP/assessment/order-rx.html", lastUpdate: "2026-07-08T17:33:56.783897" },
+  { title: "Order Slip Lab", file: "NP/assessment/order-slip-lab.html", lastUpdate: "2026-07-08T19:21:38.893409" },
+  { title: "Order Slip Rx", file: "NP/assessment/order-slip-rx.html", lastUpdate: "2026-07-08T17:30:21.286094" },
+  { title: "Search HN", file: "NP/assessment/search-hn.html", lastUpdate: "2026-07-08T11:08:11.343821" },
+];
